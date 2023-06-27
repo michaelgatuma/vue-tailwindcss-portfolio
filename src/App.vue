629b02c56d9152640658d9b07aaa7f5f42b877f9
Michael Gatuma <50529359+michaelgatuma@users.noreply.github.com>
2023-06-27 23:28:10 +0300

    It's a wrap :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,70 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from '@/assets/images/gatuma-logo.png'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="max-w-6xl mx-auto text-gray-500 py-5 transition-all">
+    <!-- Header -->
+    <div id="header" class="flex w-full justify-between">
+      <!-- Logo -->
+      <div id="logo">
+        <img class="h-10" :src="Logo" alt="" />
+      </div>
+      <!-- Nav -->
+      <div id="nav-menu" class="flex gap-10">
+        <div class="text-white">
+          <RouterLink to="/">Home</RouterLink>
+        </div>
+        <div>
+          <RouterLink to="/about">About</RouterLink>
+        </div>
+        <div>
+          <RouterLink to="/about">Works</RouterLink>
+        </div>
+        <div>
+          <RouterLink to="/about">Contact</RouterLink>
+        </div>
+      </div>
+      <!-- CTA Button -->
+      <div id="cta-btn">
+        <a
+          href="#"
+          class="bg-gray-700 text-white px-6 py-3 rounded-xl hover:bg-white hover:text-gray-700 transition-all"
+        >
+          Let's Talk
+        </a>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <!-- Router view -->
+    <div class="my-20">
+      <RouterView />
+    </div>
+    <!-- Footer -->
+    <div class="flex flex-col items-center gap-8">
+      <!-- Logo -->
+      <div id="logo">
+        <img class="h-12" :src="Logo" alt="" />
+      </div>
+      <!-- Nav -->
+      <div id="nav-menu" class="flex gap-10">
+        <div class="text-white">
+          <RouterLink to="/">Home</RouterLink>
+        </div>
+        <div>
+          <RouterLink to="/about">About</RouterLink>
+        </div>
+        <div>
+          <RouterLink to="/about">Works</RouterLink>
+        </div>
+        <div>
+          <RouterLink to="/about">Contact</RouterLink>
+        </div>
+      </div>
+      <!-- Copyright -->
+      <div class="text-gray-500">
+        © 2023 <a href="#" class="text-blue-500">Gatuma</a>. All rights reserved.
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import './assets/main.css'
+// import './assets/main.css'
+import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +7,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import VueNumber from 'vue-number-animation'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(VueNumber)
 
 app.mount('#app')
